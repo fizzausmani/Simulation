bf9e9cdf036fdfa88f71ab9a9d23814ec80e73f1
fizzausmani <99799427+fizzausmani@users.noreply.github.com>
2024-05-17 14:50:54 -0700

    removed unnecessary PDF fits

--- Nearest Neighbor Analysis.py
+++ Nearest Neighbor Analysis.py
@@ -8,9 +8,9 @@ import seaborn as sns
 from scipy.stats import poisson, chisquare
 
 #%%
-pos_f01 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/FM T5000 N625 lattice sd0.1 isotropic pos.npy')[:,:,30000]
-pos_01 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd1 isotropic pos.npy')[:,:,30000]
-pos_001 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd0.1 isotropic pos.npy')[:,:,30000]
+pos_f01 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/FM T5000 N625 lattice sd0.1 isotropic pos.npy')[:,:,40000]
+pos_01 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd1 isotropic pos.npy')[:,:,-1]
+pos_001 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd0.1 isotropic pos.npy')[:,:,-1]
 ic = pd.read_csv('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd0.1 isotropic ic.csv')
 
 N = int(ic.loc[4][1])
@@ -59,13 +59,14 @@ for i in range(0,N):
 
 def probability(counts):
     a = Counter(counts)
+    p0 = a[0]/N
     p1 = a[1]/N
     p2 = a[2]/N
     p3 = a[3]/N
     p4 = a[4]/N
     p5 = a[5]/N
     
-    prob = np.array([p1, p2, p3, p4, p5])
+    prob = np.array([p0, p1, p2, p3, p4, p5])
     
     return prob
 
@@ -75,7 +76,7 @@ probability_f01 = probability(counter_f01)
 
 #%%
 f = 14
-x1 = np.array([1,2,3,4,5])
+x1 = np.array([0,1,2,3,4,5])
 barWidth = 0.25
 x2 = [x + barWidth for x in x1]
 x3 = [x + barWidth for x in x2]
@@ -85,38 +86,35 @@ plt.clf()
 plt.bar(x1, probability_f01, hatch ="/", label = '$FM, \ell = 0.1$', color = 'black', width = barWidth)
 plt.bar(x2, probability_01, edgecolor = 'k', hatch = "//", label = '$CM, \ell H = 0.1$', color = 'red', width = barWidth)
 plt.bar(x3, probability_001, edgecolor = 'k', hatch = "//", label = '$CM, \ell H = 0.01$', color = 'coral', width = barWidth)
-plt.legend(fontsize = f)
+plt.legend(fontsize = f, loc = 'upper left')
 plt.xlabel('Number of neighbors, N', fontsize = f)
 plt.ylabel('$p(N)$', fontsize = f)
-plt.xticks([r + 1.25 for r in range(len(x1))], 
-        ['1', '2', '3', '4', '5'])
+plt.xticks([r + 0.25 for r in range(len(x1))], 
+        ['0', '1', '2', '3', '4', '5'])
 plt.savefig('Nearest Neighbor Analysis, N625_T1500.png', dpi = 300, bbox_inches = 'tight')
 plt.show()
 
 #%% poisson fit
-mu = np.mean(counter_f01)  # Poisson parameter is the mean of the data
+mu = np.mean(counter_001)  # Poisson parameter is the mean of the data
 x = np.arange(poisson.ppf(0.001, mu), poisson.ppf(0.99, mu))
 plt.cla()
 plt.plot(x, poisson.pmf(x, mu), 'r-', label='poisson pmf')
 
 # Plot your data
-plt.hist(counter_f01, bins=x, density=True, alpha=0.6, color='g')
+plt.hist(counter_001, bins=x, density=True, alpha=0.6, color='g')
 
 plt.legend()
 plt.show()
-
-#%%
-from scipy.stats import gamma
 
 # Calculate the observed frequencies
 observed_freq, bins = np.histogram(counter_001, bins=20, density=False)
 
 # Calculate the expected frequencies
-a, loc, scale = gamma.fit(counter_001)
-expected_freq = [gamma.pdf(i, a, loc, scale) * len(counter_001) for i in bins[:-1]]
+expected_freq = [poisson.pmf(i, mu) * len(counter_001) for i in range(len(bins)-1)]
 
 # Perform the chi-square test
-chi2, p = chisquare(observed_freq, f_exp=expected_freq)
+chi2, p_value = chisquare(observed_freq, f_exp=expected_freq)
 
 print(f'Chi-square statistic: {chi2}')
-print(f'p-value: {p}')
+print(f'p-value: {p_value}')
+# %%
