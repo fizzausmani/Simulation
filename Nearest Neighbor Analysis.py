06ca4561ad9598991ad6016afccca114e71377b0
fizzausmani <99799427+fizzausmani@users.noreply.github.com>
2024-05-25 02:17:19 -0700

    Mixing norm.py now has a function that works with any number of advection simulations
    Nearest Neighbor Analysis.py now contains
    lH 0.0001 and has incorporated the Fitter package,
    which returns the best probability distributions
    for a dataset. The Fitter package is not included
    in the requirements.txt file, so it must be installed
    separately.

--- Nearest Neighbor Analysis.py
+++ Nearest Neighbor Analysis.py
@@ -4,48 +4,43 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from collections import Counter
-import seaborn as sns
 from scipy.stats import poisson, chisquare
 
 #%%
 pos_f01 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/FM T5000 N625 lattice sd0.1 isotropic pos.npy')[:,:,-1]
 pos_01 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd1 isotropic pos.npy')[:,:,-1]
 pos_001 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd0.1 isotropic pos.npy')[:,:,-1]
+pos_0001 = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd0.01 isotropic pos.npy')[:,:,40000]
 ic = pd.read_csv('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd0.1 isotropic ic.csv')
 
 N = int(ic.loc[4][1])
-# T = int(ic.loc[0][1]) 
-# steps = int(ic.loc[2][1])
 
 #%%
-distances_f01 = np.zeros([N, N])
-distances_01 = np.zeros([N, N])
-distances_001 = np.zeros([N, N])
-
-indices_f01 = np.zeros([N, N])
-indices_01 = np.zeros([N, N])
-indices_001 = np.zeros([N, N])
-
 # for n in range(steps-3,steps):
-posf01 = np.transpose(pos_f01[:,:])
-pos01 = np.transpose(pos_01[:,:])
-pos001 = np.transpose(pos_001[:,:])
+posf01 = np.transpose(pos_f01)
+pos01 = np.transpose(pos_01)
+pos001 = np.transpose(pos_001)
+pos0001 = np.transpose(pos_0001)
 
 nbrs_f01 = NearestNeighbors(n_neighbors=N, algorithm='ball_tree').fit(posf01)
 nbrs_01 = NearestNeighbors(n_neighbors=N, algorithm='ball_tree').fit(pos01)
 nbrs_001 = NearestNeighbors(n_neighbors=N, algorithm='ball_tree').fit(pos001)
+nbrs_0001 = NearestNeighbors(n_neighbors=N, algorithm='ball_tree').fit(pos0001)
 
-distances_f01[:,:], indices_f01[:,:] = nbrs_001.kneighbors(posf01)
-distances_01[:,:], indices_01[:,:] = nbrs_01.kneighbors(pos01)
-distances_001[:,:], indices_001[:,:] = nbrs_001.kneighbors(pos001)
+distances_f01, indices_f01 = nbrs_001.kneighbors(posf01)
+distances_01, indices_01 = nbrs_01.kneighbors(pos01)
+distances_001, indices_001 = nbrs_001.kneighbors(pos001)
+distances_0001, indices_0001 = nbrs_0001.kneighbors(pos0001)
 
-indices_f01[:,:] = indices_f01[:,:]*(distances_f01[:,:] < 1.5)
-indices_01[:,:] = indices_01[:,:]*(distances_01[:,:] < 1.5)
-indices_001[:,:] = indices_001[:,:]*(distances_001[:,:] < 1.5)
+indices_f01 = indices_f01*(distances_f01 < 1.5)
+indices_01 = indices_01*(distances_01 < 1.5)
+indices_001 = indices_001*(distances_001 < 1.5)
+indices_0001 = indices_0001*(distances_0001 < 1.5)
 
 #%%
 counter_01 = np.zeros(N)
 counter_001 = np.zeros(N)
+counter_0001 = np.zeros(N)
 counter_f01 = np.zeros(N)
 
 # for n in range(0,steps):
@@ -53,6 +48,7 @@ for i in range(0,N):
     for j in range(0,N):
         counter_01[i] += 1*(indices_01[i,j] != 0)
         counter_001[i] += 1*(indices_001[i,j] != 0)
+        counter_0001[i] += 1*(indices_0001[i,j] != 0)
         counter_f01[i] += 1*(indices_f01[i,j] != 0)
 
 # %% probability
@@ -72,24 +68,27 @@ def probability(counts):
 
 probability_01 = probability(counter_01)
 probability_001 = probability(counter_001)
+probability_0001 = probability(counter_0001)
 probability_f01 = probability(counter_f01)
 
 #%%
 f = 14
 x1 = np.array([0,1,2,3,4,5])
-barWidth = 0.25
+barWidth = 0.20
 x2 = [x + barWidth for x in x1]
 x3 = [x + barWidth for x in x2]
+x4 = [x + barWidth for x in x3]
 plt.cla()
 plt.clf()
 
 plt.bar(x1, probability_f01, hatch ="/", label = '$FM, \ell = 0.1$', color = 'black', width = barWidth)
-plt.bar(x2, probability_01, edgecolor = 'k', hatch = "//", label = '$CM, \ell H = 0.1$', color = 'red', width = barWidth)
+plt.bar(x2, probability_01, edgecolor = 'k', hatch = "//", label = '$CM, \ell H = 0.1$', color = 'maroon', width = barWidth)
 plt.bar(x3, probability_001, edgecolor = 'k', hatch = "//", label = '$CM, \ell H = 0.01$', color = 'coral', width = barWidth)
-plt.legend(fontsize = f-2, loc = 'upper left')
+plt.bar(x4, probability_0001, edgecolor = 'k', hatch = "//", label = '$CM, \ell H = 0.001$', color = 'sandybrown', width = barWidth)
+plt.legend(fontsize = f-2, loc = 'upper right')
 plt.xlabel('Number of neighbors, N', fontsize = f)
 plt.ylabel('$p(N)$', fontsize = f)
-plt.xticks([r + 0.25 for r in range(len(x1))], 
+plt.xticks([r + 0.3 for r in range(len(x1))], 
         ['0', '1', '2', '3', '4', '5'])
 plt.savefig('Nearest Neighbor Analysis, N625_T1500.png', dpi = 300, bbox_inches = 'tight')
 plt.show()
@@ -121,6 +120,12 @@ print(f'p-value: {p_value}')
 prob = poisson.cdf(x, mu)
 np.allclose(x, poisson.ppf(prob, mu))
 # %%
-
 import seaborn as sns
 from fitter import Fitter, get_common_distributions, get_distributions
+
+f = Fitter(counter_01,
+           distributions=["burr",
+                          "cauchy"])
+f.fit()
+f.summary()
+
