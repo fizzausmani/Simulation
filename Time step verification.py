502f9cf467926734d03748334ab6b4754c09c03d
fizzausmani <99799427+fizzausmani@users.noreply.github.com>
2024-07-23 23:48:11 -0700

    file change

--- Time step verification.py
+++ Time step verification.py
@@ -2,8 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-pos_new = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T4000 N625 lattice sd0.01 isotropic testing 2 pos.npy')[:,:,-1]
-pos_old = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd0.01 isotropic pos.npy')[:,:,-1]
+pos_new = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T2000 N625 lattice sd1 isotropic testing pos.npy')[:,:,-1]
+pos_old = np.load('/Users/fizzausmani/Library/CloudStorage/Box-Box/Research/Python/Stability Analysis/CM T5000 N625 lattice sd1 isotropic pos.npy')[:,:,int(2000/0.05)]
 
 diff = pos_new - pos_old
 np.amax(diff)
